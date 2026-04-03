d912a682ba1631390e64f19084cb2fe555adfc8b
IrvinCaesar <152835521+IrvinCaesar@users.noreply.github.com>
2026-04-04 00:22:19 +0700

    Town Map Test + Chapter Start Test

---
+++ Assets/Game Logic/ChapterDialogue.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class ChapterDialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public float textSpeed;
+    public float lineDelay = 1f;
+    public string[] lines;
+
+    private int index;
+
+    void Start()
+    {
+        textComponent.text = "";
+        gameObject.SetActive(false);
+        StartDialogue();
+    }
+
+    public void StartDialogue()
+    {
+        gameObject.SetActive(true);
+        index = 0;
+        textComponent.text = "";
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        // Add a newline before every line except the first one
+        if (index > 0)
+        {
+            textComponent.text += "\n";
+        }
+
+        foreach (char c in lines[index])
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+
+        yield return new WaitForSeconds(lineDelay);
+
+        if (index < lines.Length - 1)
+        {
+            index++;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            yield return new WaitForSeconds(1f);
+            gameObject.SetActive(false);
+        }
+    }
+}
