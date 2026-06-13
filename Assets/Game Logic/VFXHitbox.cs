34cc4231342afe5c44cbe0a0870ff93d6776fcee
IrvinCaesar <152835521+IrvinCaesar@users.noreply.github.com>
2026-06-13 22:47:24 +0700

    Ult ability fix

---
+++ Assets/Game Logic/VFXHitbox.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class VFXHitbox : MonoBehaviour
+{
+    public int damage = 5;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Entity hitEnemy = other.GetComponent<Entity>();
+
+        if (hitEnemy != null && !other.CompareTag("Player"))
+        {
+            hitEnemy.TakeDamage(damage);
+            Debug.Log("MASSIVE HITBOX TOUCHED: " + other.name);
+        }
+    }
+}
