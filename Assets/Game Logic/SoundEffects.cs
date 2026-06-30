e447a2735a877d3a51d06845c7ff61aa4a960478
Winstonjie <79020292+MrWinstonjie@users.noreply.github.com>
2026-06-30 09:37:58 +0700

    hrtrh

--- Assets/Game Logic/SoundEffects.cs
+++ Assets/Game Logic/SoundEffects.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class SoundEffects : MonoBehaviour
 {
-    
+    public AudioSource src;
+    public AudioClip musik;
+
+
     void Start()
     {
+        src.clip = musik;
+        src.Play();
         
     }
 
